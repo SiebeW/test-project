e21fed6020ff137532fcb76bedf05ecf0ce92036
SiebeW <siebe.wierdsma@gmail.com>
2024-03-23 14:47:53 +0100

    basic pokemon list

---
+++ src/components/PokemonList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="pkmn in pokemon" :key="pkmn.name">
+                {{ pkmn.name }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import Pokedex from 'pokedex-promise-v2';
+const P = new Pokedex();
+const original151 = {
+    limit: 151,
+    offset: 0
+}
+
+export default {
+    data() {
+        return {
+            pokemon: [],
+        };
+    },
+    async created() {
+        try {
+            const response = await P.getPokemonsList(original151);
+            this.pokemon = response.results;
+        } catch (error) {
+            console.error(error);
+        }
+    },
+};
+</script>
+
+<style scoped></style>
