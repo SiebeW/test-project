d3b3759f5522ca653f93a84e3a207ca00379aacd
SiebeW <siebe.wierdsma@gmail.com>
2024-03-26 11:03:15 +0100

    loading spinner

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,23 @@ import { TypeColorsLighter } from "@/stores/TypeColors";
 PokemonStore.fetchPokemon();
 
 function getTypeColor() {
-  if (PokemonStore.selectedPokemon.types)
-  {return TypeColorsLighter[PokemonStore.selectedPokemon.types[0].type.name]}
+  if (PokemonStore.selectedPokemon.types) { return TypeColorsLighter[PokemonStore.selectedPokemon.types[0].type.name] }
 }
 </script>
 
-<template >
+<template>
+  <div
+    class="spinner-container flex justify-content-center justify-items-center align-items-center align-content-center"
+    v-if="PokemonStore.pokemon.length === 0">
+    <svg class="spinner" version="1.1" id="Layer_2_00000125588937344236749390000004514562173039608716_"
+      xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+      viewBox="0 0 13.1 13.6" style="enable-background:new 0 0 13.1 13.6;" xml:space="preserve">
+      <path class="pb0" d="M6.6,12.4c3.1,0,5.6-2.5,5.6-5.6H1C1,9.9,3.5,12.4,6.6,12.4z" />
+      <path class="pb0" d="M6.6,1.2C3.5,1.2,1,3.7,1,6.8h11.1C12.1,3.7,9.6,1.2,6.6,1.2z" />
+      <circle class="pb1" cx="6.6" cy="6.8" r="1.8" />
+    </svg>
+
+  </div>
   <div class="flex row" :style="'background-color: ' + getTypeColor()">
     <MainMenu />
     <main>
@@ -22,47 +33,86 @@ function getTypeColor() {
 </template>
 
 <style>
-h1, h2, h3, h4 {
+.pb0 {
+  fill: none;
+  stroke: #8B8B90;
+  stroke-width: 1.5;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  stroke-miterlimit: 10;
+}
+
+.pb1 {
+  fill: #FFFFFF;
+  stroke: #8B8B90;
+  stroke-miterlimit: 10;
+}
+
+.spinner-container {
+  z-index: 50;
+  background-color: rgba(255, 255, 255, 0.75);
+}
+
+.spinner {
+  animation: rotation 1s linear infinite;
+}
+
+h1,
+h2,
+h3,
+h4 {
   user-select: none;
 }
+
 .pkmn-type {
-    padding: 0.25em 1em 0.35em;
-    border-radius: 1em;
-    color: white;
-    font-size: 0.9em;
+  padding: 0.25em 1em 0.35em;
+  border-radius: 1em;
+  color: white;
+  font-size: 0.9em;
 }
+
 body {
   font-family: Arial, Helvetica, sans-serif;
   margin: 0;
 }
+
 .flex {
-    display: flex;
+  display: flex;
 }
+
 .row {
-    flex-direction: row;
+  flex-direction: row;
 }
+
 .column {
   flex-direction: column;
 }
+
 .text-white {
   color: white;
 }
+
 .p-1 {
   padding: 1em;
 }
+
 .text-bold {
-    font-weight: bold;
+  font-weight: bold;
 }
+
 .ml-auto {
   justify-self: flex-end;
   margin-left: auto;
 }
+
 .align-items-center {
   align-items: center;
 }
+
 .align-self-start {
   align-self: flex-start;
 }
+
 .justify-content-end {
   justify-content: end;
 }
--- src/views/MainMenu.vue
+++ src/views/MainMenu.vue
@@ -8,8 +8,8 @@ import { PokemonStore } from "@/stores/PokemonStore";
   <nav class="flex column">
     <div class="p-1">
       <div class="filter-sort flex row justify-content-end">
-        <svg id="Layer_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11.24 12.66"><g id="Layer_5"><polyline class="cls-1" points="2.62 .75 10.49 .75 10.49 3.15 7.22 6.42 7.22 10.58 4.22 11.91 4.22 6.54 .75 3.07 .75 .75"/></g></svg>
-        <svg @click="PokemonStore.sortPokemon()" id="Layer_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12.64 12.38"><g id="Layer_4"><polyline class="cls-1" points=".75 3.3 3.3 .75 5.73 3.18"/><line class="cls-1" x1="3.3" y1="11.63" x2="3.3" y2=".75"/><polyline class="cls-1" points="11.89 9.08 9.34 11.63 6.91 9.2"/><line class="cls-1" x1="9.34" y1=".75" x2="9.34" y2="11.63"/></g></svg>
+        <svg id="filter-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11.24 12.66"><g id="Layer_5"><polyline class="cls-1" points="2.62 .75 10.49 .75 10.49 3.15 7.22 6.42 7.22 10.58 4.22 11.91 4.22 6.54 .75 3.07 .75 .75"/></g></svg>
+        <svg @click="PokemonStore.sortPokemon()" id="sort-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12.64 12.38"><g id="Layer_4"><polyline class="cls-1" points=".75 3.3 3.3 .75 5.73 3.18"/><line class="cls-1" x1="3.3" y1="11.63" x2="3.3" y2=".75"/><polyline class="cls-1" points="11.89 9.08 9.34 11.63 6.91 9.2"/><line class="cls-1" x1="9.34" y1=".75" x2="9.34" y2="11.63"/></g></svg>
       </div>
       <h1>Pokédex</h1>
       <PokemonSearch></PokemonSearch>
@@ -43,6 +43,10 @@ nav {
   max-width: 20px;
   cursor: pointer;
   opacity: 0.5;
+}
+.filter-icon, .filter-icon:hover {
+  cursor: not-allowed;
+  opacity: 0.12;
 }
 .cls-1 {
   fill: none;
